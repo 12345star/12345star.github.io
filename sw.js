28f612d4f27af166abfbb64b2345236ac419c8bc
David Salas <yoansalas76@gmail.com>
2026-04-21 22:43:52 -0600

    agregando nuevos formatos de img goland webp

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const dataCacheName = 'KRIPTON-data';
-const cacheName = 'KRIPTON-v4'; // 👈 cambia la versión cuando actualices archivos
+const cacheName = 'KRIPTON-v5'; // 👈 cambia la versión cuando actualices archivos
 
 // Install: guarda los archivos iniciales
 self.addEventListener('install', event => {
