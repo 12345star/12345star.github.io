dfd21ee72aeb928da31f42f0c3a84353cb8666f9
David Salas Lorente <yoansalas76@gmail.com>
2026-06-17 18:15:09 +0000

    incluyendo lo de WEB MCP

--- app.js
+++ app.js
@@ -46,3 +46,64 @@
         console.error("Error solicitando permiso:", err);
       }
     }
+
+
+  // Herramienta: Servicios MCP
+  document.modelContext.registerTool({
+    name: "getServices",
+    title: "Lista de servicios",
+    description: "Devuelve los servicios ofrecidos en la página Developer de Costa Rica",
+    inputSchema: { type: "object", properties: {} },
+    execute: async () => {
+      return {
+        services: [
+          "QA Analyst",
+          "Developer Costa Rica",
+          "Developer JAVA",
+          "Developer Python",
+          "Developer Node.js",
+          "Developer PHP",
+          "Developer C#",
+          "Developer C++"
+
+        ]
+      };
+    }
+  });
+
+  // Herramienta: Proyectos. para MCP
+  document.modelContext.registerTool({
+    name: "getProjects",
+    title: "Portafolio de proyectos",
+    description: "Devuelve los proyectos personales del desarrollador",
+    inputSchema: { type: "object", properties: {} },
+    execute: async () => {
+      return {
+        projects: [
+          { name: "Golang + calculadora terminal",  url: "https://12345star.github.io/calculadora-golang/" },
+          { name: "HTML,CSS + Template Portafolio", url: "https://12345star.github.io/template_protafolio/" },
+          { name: "Python + crear IMG para movil",  url: "https://12345star.github.io/convertidor-imagen-table-movil-python/" },
+          { name: "Python + calculadora terminal",  url: "https://12345star.github.io/calculadora-terminal-python/"},
+          { name: "Pyton + Calculadora Flet",       url: "https://12345star.github.io/flet-python-calculator" },
+          { name: "Python + Creador de Curriculum", url: "https://12345star.github.io/curriculum-python-flet/" },
+          { name: "Python,docker,postgres,fastapi", url: "https://12345star.github.io/docker-postgres-python-fastapi/"},
+          { name: "Portafolio personal WEB",        url: "https://12345star.github.io"}
+        ]
+      };
+    }
+  });
+
+  // Herramienta: Contacto para MCP
+  document.modelContext.registerTool({
+    name: "getContactInfo",
+    title: "Información de contacto",
+    description: "Devuelve email y redes sociales",
+    inputSchema: { type: "object", properties: {} },
+    execute: async () => {
+      return {
+        github: "https://github.com/12345star",
+        linkedin: "https://www.linkedin.com/in/david-salas-lorente-757947198/"
+      };
+    }
+  });
+
